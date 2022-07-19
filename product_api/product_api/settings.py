59bff6b52079b3c22fa5d7302b13078c2be2d643
Kenneth Kent Flores <floresjohnkent@gmail.com>
2022-07-19 20:15:39 +0800

    Fix

--- product_api/product_api/settings.py
+++ product_api/product_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-57)5io-8u8kn&o!&x!29&hnl-z=eifw$c@8@o%1za!9xac93_h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
